e3bf3fddeb486e75978395dd0350c9800ed830de
jurgen1c <jurgen1c@gmail.com>
2020-10-03 12:33:16 -0600

    Add route root for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  authenticated :user do
+    root to: "docs#index", as: :authenticated_root
+  end
+
+
   resources :docs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
