cf8a4abc6376e81df8741f5d211894f128a29abc
jurgen1c <jurgen1c@gmail.com>
2020-10-03 12:09:51 -0600

    Change index to current user docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -2,7 +2,7 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
-    @docs = Doc.all.order("created_at DESC")
+    @docs = Doc.where(user_id: current_user)
   end
 
   def show
