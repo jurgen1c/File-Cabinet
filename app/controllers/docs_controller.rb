bd4917d95177bb7ee40ed539fcbb95b724187255
jurgen1c <jurgen1c@gmail.com>
2020-10-03 11:53:44 -0600

    Finish doc views and actions & install devise

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,5 @@
 class DocsController < ApplicationController
-  before_action :find_doc, only: [:show, :edit, :destroy]
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
     @docs = Doc.all.order("created_at DESC")
@@ -27,9 +27,16 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
